54097875d62f59fa35b98fe77d731f775637a814
Ed Mechem <ed@mechem.org>
2018-07-31 13:18:48 -0700

    Cleanup routes.rb by not requiring 'active_storage/engine'

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,28 @@
 require_relative 'boot'
 
-require 'rails/all'
+
+# require 'rails/all'
+
+# Don't require all - manually add each railtie instead, so as to not load
+# active_storage/engine. Thus, clean up the routes.rb file and don't show the
+# unneeded entried associated with active storage.
+# As per:
+# https://mikerogers.io/2018/04/13/remove-activestorage-from-rails-5-2.html
+
+require "rails"
+
+# Include each railties manually, excluding `active_storage/engine`
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
